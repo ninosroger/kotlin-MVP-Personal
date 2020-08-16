10459de3c103b630211e3e61cb4a9d6f4cb14b52
ninosroger <ninos_r@foxmail.com>
2020-08-16 10:52:30 +0800

    完善dp和px计算精度浮点问题

--- app/src/main/java/com/ninos/mvp/utils/ScreenUtils.kt
+++ app/src/main/java/com/ninos/mvp/utils/ScreenUtils.kt
@@ -2,9 +2,17 @@ package com.ninos.mvp.utils
 
 import android.content.Context
 
+/**
+ * @author Ninos
+ *
+ * 不管是dp2px还是px2dp 因为结果做了强转会丢失精度，预期4.9应该四舍五入为5，但以下结果会是4
+ * 4.1f.toInt() + "---" + 4.9f.toInt()
+ * 4---4
+ */
+
 object ScreenUtils {
     /**
-     * dp -> px
+     * dp转px
      */
     fun dp2px(context: Context, dp: Float): Int {
         val density = context.resources.displayMetrics.density
@@ -12,10 +20,10 @@ object ScreenUtils {
     }
 
     /**
-     * px -> dp
+     * px转dp
      */
-    fun px2dp(context: Context, px: Int): Float {
+    fun px2dp(context: Context, px: Int): Int {
         val density = context.resources.displayMetrics.density
-        return px / density
+        return (px / density + 0.5f).toInt()
     }
 }
